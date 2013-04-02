05a23bef613cc9b183fa88600b8fc6f0d08efcf6
furugomu <furugomu@gmail.com>
2013-04-02 20:46:10 +0900

    あいかつ

---
+++ aical.rb
@@ -0,0 +1,114 @@
+#!/usr/bin/env ruby
+# -*- encoding: UTF-8 -*-
+
+require 'nokogiri'
+require 'uri'
+require 'json'
+
+class Aical
+  attr_accessor :year, :month, :day
+  def initialize
+    @schedules = []
+  end
+
+  def feed(doc)
+    container = doc.at_css(".info-schedule")
+    container.css('table').each do |table|
+      feed_table(table)
+    end
+  end
+
+  def feed_table(table)
+    # 年と月
+    text = table.at_css('th').text
+    m = text.match(/(\d+)年(\d+)月/) or raise ValueError, text
+    @year = m[1].to_i
+    @month = m[2].to_i
+
+    table.css('tr')[1..-1].each do |tr|
+      feed_row(tr)
+    end
+  end
+
+  def feed_row(tr)
+    # 日付
+    text = tr.at_css('td').text
+    m = text.match(/(\d+)日/) or raise ValueError, text
+    @day = m[1].to_i
+
+    tr.css('p').each do |p|
+      feed_item(p)
+    end
+  end
+
+  def feed_item(p)
+    @schedules << parse_item(p)
+  end
+
+  def parse_item(p)
+    # 日付
+    text = p.text
+    re = /(?:　※)?(\d+)年(\d+)月(\d+)日～(?:(\d+)年)?(\d+)月(\d+)日/
+    m = text.match(re)
+    if m
+      year_until = (m[4] || m[1]).to_i
+      date_from = Time.local(m[1].to_i, m[2].to_i, m[3].to_i)
+      date_until = Time.local(year_until, m[5].to_i, m[6].to_i)
+    else
+      date_from = date_until = Time.local(@year, @month, @day)
+    end
+    # 日付をとっぱらう
+    text = text.sub(re, '')
+
+    # URL
+    url = nil
+    if (a = p.at_css('a[href]'))
+      base = 'http://www.aikatsu.com/calender/'
+      url = URI.join(base, a.attr(:href)).to_s
+    end
+
+    # type
+    type = class_to_type(p.attr(:class))
+
+    {
+      'type' => type,
+      'date_from' => date_from,
+      'date_until' => date_until,
+      'content' => text.strip,
+      'link' => url,
+    }
+  end
+
+  def class_to_type(s)
+    if s =~ /schedule-(\w+)/
+      $1
+    else
+      nil
+    end
+  end
+
+  def to_json(pretty=false)
+    if pretty
+      JSON.pretty_generate(@schedules)
+    else
+      JSON.dump(@schedules)
+    end
+  end
+end
+
+def main
+  aical = Aical.new
+  doc = 
+    if ARGV[0]
+      require 'open-uri'
+      open(ARGV[0]) {|f| Nokogiri::HTML.parse(f) }
+    else
+      Nokogiri::HTML.parse($stdin)
+    end
+  aical.feed(doc)
+  puts aical.to_json(true)
+end
+
+if __FILE__ == $0
+  main
+end
---
+++ aical_spec.rb
@@ -0,0 +1,120 @@
+#!ruby
+# -*- encoding: UTF-8 -*-
+
+require './aical'
+
+def html(s)
+  Nokogiri::HTML::DocumentFragment.parse(s.strip).children.first
+end
+
+describe Aical do
+  let(:aical) { Aical.new }
+  subject { aical }
+  before do
+    aical.year = 2000; aical.month = 10; aical.day = 10
+  end
+
+  describe 'feed_table' do
+    let(:table) do
+      html(<<-HTML)
+        <table cellspacing="0" class="apr">
+        <tbody>
+        	<tr>
+        		<th colspan="3">2013年4月</th>
+        	</tr>
+        </tbody>
+        </table>
+      HTML
+    end
+    before do
+      aical.feed_table(table)
+    end
+    its(:year) { should == 2013 }
+    its(:month) { should == 4 }
+  end
+
+  describe 'feed_row' do
+    let(:tr) do
+      html(<<-HTML)
+        <tr>
+        <td class="schedule-day"><span>03日</span><span class="schedule-week">水</span></td>
+        <td><p>x</p></td>
+        </tr>
+      HTML
+    end
+    before do
+      aical.feed_row(tr)
+    end
+    its(:day) { should == 3 }
+  end
+
+  describe 'parse_item' do
+    let(:p) { html('<p class="schedule-game clearfix">いちごちゃん</p>') }
+    subject { aical.parse_item(p) }
+
+    it 'content' do
+      subject['content'].should == p.text
+    end
+    it 'date_from' do
+      subject['date_from'].should == Time.local(aical.year, aical.month, aical.day)
+    end
+    it 'date_until' do
+      subject['date_until'].should == Time.local(aical.year, aical.month, aical.day)
+    end
+    it 'type' do
+      subject['type'].should == 'game'
+    end
+
+    context '中に日付が書かれている(※あり)' do
+      let(:p) { html('<p class="schedule-game">あおいちゃん　※2013年2月14日～2014年3月31日</p>') }
+      it 'content' do
+        subject['content'].should == 'あおいちゃん'
+      end
+      it 'date_from' do
+        subject['date_from'].should == Time.local(2013, 2, 14)
+      end
+      it 'date_until' do
+        subject['date_until'].should == Time.local(2014, 3, 31)
+      end
+    end
+
+    context '中に日付が書かれている(終了年が無い)' do
+      let(:p) { html('<p class="schedule-game">蘭ちゃん　※2013年2月14日～3月31日</p>') }
+      it 'content' do
+        subject['content'].should == '蘭ちゃん'
+      end
+      it 'date_from' do
+        subject['date_from'].should == Time.local(2013, 2, 14)
+      end
+      it 'date_until' do
+        subject['date_until'].should == Time.local(2013, 3, 31)
+      end
+    end
+
+    context '中に日付が書かれている(※なし)' do
+      let(:p) do
+        html(<<-HTML)
+          <p class="schedule-game">
+          <span>2013年2月14日～2014年3月31日</span>
+          おとめちゃん</p>
+        HTML
+      end
+      it 'content' do
+        subject['content'].should == 'おとめちゃん'
+      end
+      it 'date_from' do
+        subject['date_from'].should == Time.local(2013, 2, 14)
+      end
+      it 'date_until' do
+        subject['date_until'].should == Time.local(2014, 3, 31)
+      end
+    end
+
+    context 'リンクがある' do
+      let(:p) { html('<p class="schedule-magazine"><a href="../magazine/magazine15.html">ちゃお4月号</a></p>') }
+      it 'link' do
+        subject['link'].should == 'http://www.aikatsu.com/magazine/magazine15.html'
+      end
+    end
+  end
+end
